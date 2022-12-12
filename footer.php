33d5302013e806550b90654c083f62d270707f73
tmanauv <120377669+tmanauv@users.noreply.github.com>
2022-12-12 22:10:15 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,33 @@
+   <!-- footer section --> 
+			<footer>
+				<div class="col-md-12 fsection">
+					<div class="col-md-4" id="contact">
+					<p>Category</p> <hr>
+					<ul>
+						<li><a href="signin.php">Contact with Doctors</a></li>
+						<li><a href="signin.php">Request an Appointment</a></li>
+                                                
+						
+					</ul>
+					</div>
+					<div class="col-md-4">
+						<p> Need Help ?</p> <hr>
+						<p> Health Group <br>
+							health.support@gmail.com <br>
+							Cell: +91987654321</p>
+							<span style="color: red;font-size: 15px">&copy;<?php echo date('Y'); ?>-All Rights Reserved </span>
+					</div>
+					<div class="col-md-4 share_img">
+					<p>About us</p> <hr>
+                                                <p> This is an online application, where you can get a confirmed appointment of your docter. You can book an appointment based upon the availability of the doctor & we will send you status notifications of the chamber.</p>
+						<a href="" ><img src="img/fb-free.png" alt="facebok"></a>
+						<a href=""><img src="img/gogle-plud-free.png" alt="google-plus"></a>
+						<a href=""><img src="img/twitter.png" alt="twitter"></a>
+						
+					</div>
+				</div>
+				
+
+			</footer>
+
+		<!-- footer section Ends--> 
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php include('header.php'); ?>
+
+	<!-- this is for welcome -->
+	<div class="content">
+		<article>
+			<!-- <img src="photo/background.jpg" alt=""> -->
+			<p class="text-justify">Blood is universally recognized as the most precious element that sustains life. It saves innumerable lives across the world in a variety of conditions. The need for blood is great - on any given day, approximately 39,000 units of Red Blood Cells are needed. More than 29 million units of blood components are transfused every year. Donate Blood Despite the increase in the number of donors, blood remains in short supply during emergencies, mainly attributed to the lack of information and accessibility. We positively believe this tool can overcome most of these challenges by effectively connecting the blood donors with the blood recipients.
+			</p>
+		</article>
+
+
+	
+	</div><br>
+
+	<!-- nivo slider starts -->
+	<div class="col-md-12 sliderImg">
+		<img src="img/doctor5.jpg" alt="">
+	</div>
+	<!-- nivo slider ends -->
+
+	<!-- main Content -->
+	<div class="main_content">
+		<div class="col-md-8">
+			<article>
+			<h3 style="font-weight: bold;font-family:inherit;">Finds Doctors from anywhere anytime!</h3><hr>
+				<p class="text-justify"> We are designed to meet the national and international healthcare standards and is committed to providing quality medical care and excellent patient service. The facility brings together an expert team of superspecalist doctors who work together with a compassionate nursing staff to provide patients accurate diagonisis and medical care. The hospitals has high-end technological infrastructure and medical facilities including advanced Operation Theaters, Digital X-Ray, and Critical Care Units.
+				</p>
+			</article>
+		</div>
+		<div class="col-md-4">
+			<h3 class="text-center" style="font-weight:bold;font-family:inherit;">Doctors Appoinment...?</h3><hr>
+			<ul class="text-justify">
+				We value our patient's saftey and so we follow high standards of medical and surgical protocols. We always strive to provide the best possible medical treatment to our patient aiming to achieve highest success rate. Just book an appointment and consult the best doctors in India. 
+			</ul>
+		</div>
+
+          
+    </div>
+
+ 	<?php include('footer.php'); ?>
+
+
+	
+</div>	<!--  containerFluid Ends -->
+
+
+
+	<script src="js/jquery-1.9.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	 
+	
+</body>
+</html>
